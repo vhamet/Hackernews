b2c5b499c50e5804a3c141c474c468555da43e9e
valentin <valentin.hamet@gmail.com>
2019-06-05 15:35:30 +0200

    init

---
+++ hackernews-frontend/src/components/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import LinkList from './LinkList';
+import CreateLink from './CreateLink';
+import Header from './Header';
+import Login from './Login';
+import Search from './Search';
+import '../style/App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path='/search' component={Search} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
