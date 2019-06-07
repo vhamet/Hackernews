69ecdb37b927f639c416634a1a602591d2939cfc
valentin <valentin.hamet@gmail.com>
2019-06-07 14:46:47 +0200

    tutorial backend

--- hackernews-frontend/src/components/Header.js
+++ hackernews-frontend/src/components/Header.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { withRouter } from 'react-router';
-import { AUTH_TOKEN } from '../constants';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router";
+import { AUTH_TOKEN } from "../constants";
 
 class Header extends Component {
   render() {
@@ -14,6 +14,10 @@ class Header extends Component {
             new
           </Link>
           <div className="ml1">|</div>
+          <Link to="/top" className="ml1 no-underline black">
+            top
+          </Link>
+          <div className="ml1">|</div>
           <Link to="/search" className="ml1 no-underline black">
             search
           </Link>
@@ -23,22 +27,22 @@ class Header extends Component {
           </Link>
         </div>
         <div className="flex flex-fixed">
-        {authToken ? (
-          <div
-            className="ml1 pointer black"
-            onClick={() => {
-              localStorage.removeItem(AUTH_TOKEN)
-              this.props.history.push(`/`)
-            }}
-          >
-            logout
-          </div>
-        ) : (
-          <Link to="/login" className="ml1 no-underline black">
-            login
-          </Link>
-        )}
-      </div>
+          {authToken ? (
+            <div
+              className="ml1 pointer black"
+              onClick={() => {
+                localStorage.removeItem(AUTH_TOKEN);
+                this.props.history.push(`/`);
+              }}
+            >
+              logout
+            </div>
+          ) : (
+            <Link to="/login" className="ml1 no-underline black">
+              login
+            </Link>
+          )}
+        </div>
       </div>
     );
   }
